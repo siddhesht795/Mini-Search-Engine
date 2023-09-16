bb1cfe04dbebb9df081eefa3a61679807c715bdc
siddhesht795 <140600638+siddhesht795@users.noreply.github.com>
2023-09-16 18:59:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const search = ()=> {
+	let searchbox = document.getElementById("search-item").value.toUpperCase();
+	let storeitems = document.getElementById("product-list");
+	let product = document.querySelectorAll(".product");
+	let pname = document.getElementsByTagName("h2");
+
+	for(var i=0;i < pname.length; i++) {
+		let match = product[i].getElementsByTagName("h2")[0];
+
+		if(match) {
+			let textvalue = match.textContent || match.innerHTML;
+
+			if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+				product[i].style.display = "";
+			}
+			else {
+				product[i].style.display = "none";
+			}
+		}
+	}
+}
